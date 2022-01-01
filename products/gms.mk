1ab146576aedee916f092707d71a312d3c1e110a
TheScarastic <warabhishek@gmail.com>
2022-01-01 17:47:19 +0530

    gms: Add props for gapps

--- products/gms.mk
+++ products/gms.mk
@@ -14,4 +14,39 @@
 # limitations under the License.
 #
 
+# Gboard Props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.ime.bs_theme=true \
+    ro.com.google.ime.theme_id=5 \
+    ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
+
+# GMS Props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.opa.eligible_device=true
+
+# SetupWizard Props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.esim_cid_ignore=00000001 \
+    setupwizard.enable_assist_gesture_training=true \
+    setupwizard.feature.baseline_setupwizard_enabled=true \
+    setupwizard.feature.day_night_mode_enabled=true \
+    setupwizard.feature.enable_quick_start_flow=true \
+    setupwizard.feature.enable_restore_anytime=true \
+    setupwizard.feature.lifecycle_refactoring=true \
+    setupwizard.feature.notification_refactoring=true \
+    setupwizard.feature.portal_notification=true \
+    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
+    setupwizard.feature.show_pixel_tos=true \
+    setupwizard.feature.show_support_link_in_deferred_setup=false \
+    setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
+    setupwizard.personal_safety_suw_enabled=true \
+    setupwizard.theme=glif_v4 \
+    setupwizard.feature.enable_wifi_tracker=true
+
+# StorageManager configuration
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.storage_manager.enabled=false \
+    ro.storage_manager.show_opt_in=false
+
 $(call inherit-product, vendor/gms/common/common-vendor.mk)
